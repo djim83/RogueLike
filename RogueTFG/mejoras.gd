f8172f64d4b08bb4a6a79fc04f7d610189b6fc77
David <davidjim83@icloud.com>
2025-11-13 18:47:59 +0100

    Implementando mejoras entre niveles

---
+++ RogueTFG/mejoras.gd
@@ -0,0 +1,51 @@
+extends Control
+
+@export var next_scene_path: String = "res://Escenas/Nivel2.tscn"
+
+@onready var btn_vida: Button = $VBoxContainer/Button_Vida
+@onready var btn_municion: Button = $VBoxContainer/Button_Municion
+@onready var btn_velocidad: Button = $VBoxContainer/Button_Velocidad
+@onready var btn_cadencia: Button = $VBoxContainer/Button_Cadencia
+
+func _ready() -> void:
+	btn_vida.pressed.connect(_on_btn_vida)
+	btn_municion.pressed.connect(_on_btn_municion)
+	btn_velocidad.pressed.connect(_on_btn_velocidad)
+	btn_cadencia.pressed.connect(_on_btn_cadencia)
+
+func _on_btn_vida() -> void:
+	var antes = PlayerStats.max_vida
+	PlayerStats.max_vida += 1
+	PlayerStats.vida = PlayerStats.max_vida
+	var despues = PlayerStats.max_vida
+	print("Mejora: Vida. Vida máxima pasa de %d a %d" % [antes, despues])
+	_go_next()
+
+func _on_btn_municion() -> void:
+	var antes = PlayerStats.municion_pistola
+	PlayerStats.municion_pistola += 50
+	var despues = PlayerStats.municion_pistola
+	print("Mejora: Munición. Munición pasa de %d a %d" % [antes, despues])
+	_go_next()
+
+func _on_btn_velocidad() -> void:
+	var antes = PlayerStats.velocidad
+	PlayerStats.velocidad *= 1.20
+	var despues = PlayerStats.velocidad
+	print("Mejora: Velocidad. Velocidad pasa de %f a %f" % [antes, despues])
+	_go_next()
+
+func _on_btn_cadencia() -> void:
+	var antes = PlayerStats.velocidad_disparo
+	PlayerStats.velocidad_disparo = max(0.02, PlayerStats.velocidad_disparo * 0.8)
+	var despues = PlayerStats.velocidad_disparo
+	print("Mejora: Cadencia. Velocidad de disparo pasa de %f a %f" % [antes, despues])
+	_go_next()
+
+func _go_next() -> void:
+	# Cargamos siguiente escena (Nivel2)
+	if next_scene_path != "":
+		print("Cargando siguiente escena:", next_scene_path)
+		get_tree().change_scene_to_file(next_scene_path)
+	else:
+		queue_free()
--- RogueTFG/puerta.gd
+++ RogueTFG/puerta.gd
@@ -6,4 +6,4 @@ func _ready():
 func _on_body_entered(body):
 	if body.is_in_group("Jugador"):
 		print("Vamos a la fase siguiente")
-		get_tree().change_scene_to_file("res://Escenas/nivel2.tscn")
+		get_tree().change_scene_to_file("res://Escenas/Mejoras.tscn")
