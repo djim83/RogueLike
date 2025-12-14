97f2f15bdc287bd86915d2008d5e59efebe29d97
David <davidjim83@icloud.com>
2025-12-14 18:38:19 +0100

    Corregir bugs

--- RogueTFG/puerta.gd
+++ RogueTFG/puerta.gd
@@ -2,27 +2,36 @@ extends Area2D
 
 @onready var anim := $AnimatedSprite2D
 
+var usada := false
+
 func _ready():
 	add_to_group("Puerta")
-	anim.play() 
+	anim.play()
 	body_entered.connect(_on_body_entered)
 
-
 func _on_body_entered(body):
+	if usada:
+		return
+
 	if not body.is_in_group("Jugador"):
 		return
 
+	usada = true
+	monitoring = false
+
 	print("Entrando por la puerta")
 
+	call_deferred("_cambiar_escena")
+
+func _cambiar_escena():
 	var escena_actual := get_tree().current_scene.scene_file_path
-	var siguiente_escena: Node
 
 	if escena_actual.ends_with("nivelBoss.tscn"):
-		siguiente_escena = load("res://Escenas/escenaFinal.tscn").instantiate()
+		get_tree().change_scene_to_file("res://Escenas/escenaFinal.tscn")
 	else:
-		siguiente_escena = load("res://Escenas/Mejoras.tscn").instantiate()
-		siguiente_escena.previous_scene_path = escena_actual
+		var mejoras_scene: Node = load("res://Escenas/Mejoras.tscn").instantiate()
+		mejoras_scene.previous_scene_path = escena_actual
 
-	get_tree().root.add_child(siguiente_escena)
-	get_tree().current_scene.queue_free()
-	get_tree().current_scene = siguiente_escena
+		get_tree().root.add_child(mejoras_scene)
+		get_tree().current_scene.queue_free()
+		get_tree().current_scene = mejoras_scene
